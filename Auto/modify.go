17549caf80cd8a9709a5fe0f871f41af4f45e31d
bbantu <brianbaraza40@gmail.com>
2024-07-05 00:21:33 +0300

    feat: Implement Modify function to autocorrect text
    - Add Modify function to transform text based on specific rules.
    - Split input text into words using strings.Fields.
    - Iterate through words and check for the "(up)" command.
    - Convert the previous word to uppercase if "(up)" is found.
    - Remove the "(up)" command from the list of words.
    - Join the modified words back into a single string and return the result.
    - Include debug print statement to output the list of words.

---
+++ Auto/modify.go
@@ -0,0 +1,22 @@
+package Auto
+
+import (
+	"strings"
+	"fmt"
+)
+
+func Modify(text string) string {
+	words := strings.Fields(text)
+	fmt.Println(words)
+
+	for i := 0; i < len(words); i++ {
+		word := words[i]
+
+		switch word {
+		case "(up)":
+			words[i-1] = strings.ToUpper(words[i-1])
+			words = append(words[:i], words[i+1:]...)
+		}
+	}
+	return strings.Join(words, " ")
+}
